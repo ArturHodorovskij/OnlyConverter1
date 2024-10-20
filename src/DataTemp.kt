45aedc0c92d5cc89c75b36bbf0d450b51cbd6ced
ArturHodorovskij <tjarturtj@gmail.com>
2024-10-20 12:10:39 +0300

    Ref

--- src/DataTemp.kt
+++ src/DataTemp.kt
@@ -1,11 +1,11 @@
-class DataTemp(putTempName: Double) {
-    val degrtodegr = putTempName
-    val degrtokalv = (putTempName + 273.15)
-    val degrtofar = (putTempName * 1.8 + 32)
-    val kalvtodegr = (putTempName - 273.15)
-    val kalvtokalv = putTempName
-    val kalvtofar = ((putTempName - 273.15) * 1.8000 + 32.00)
-    val fartodegr = ((putTempName - 32) / 1.8)
-    val fartokalv = ((putTempName - 32) / 1.8 + 237.15)
-    val fartofar = putTempName
+class DataTemp(inputTempValue: Double) {
+    val degrtodegr = inputTempValue
+    val degrtokalv = (inputTempValue + 273.15)
+    val degrtofar = (inputTempValue * 1.8 + 32)
+    val kalvtodegr = (inputTempValue - 273.15)
+    val kalvtokalv = inputTempValue
+    val kalvtofar = ((inputTempValue - 273.15) * 1.8000 + 32.00)
+    val fartodegr = ((inputTempValue - 32) / 1.8)
+    val fartokalv = ((inputTempValue - 32) / 1.8 + 237.15)
+    val fartofar = inputTempValue
 }
